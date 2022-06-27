9d607013fbac1c21f2d5acd130568efe0b470eb5
Omolemo Tshwaolesele <omolemotswaolesele@gmail.com>
2022-06-27 19:20:06 +0200

    styling and projects and footer to be done

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Technologies from './componets/technologies';
 function App() {
   return (  
     
-    <div class="px-2 sm:px-8 md:px-24 lg:px-48 xl:px-72">
+    <div class="px-2 sm:px-8 md:px-24 lg:px-48 xl:px-72 text-white">
   
           <div	 >
                <Nav />
--- src/componets/technologies.jsx
+++ src/componets/technologies.jsx
@@ -1,13 +1,38 @@
 import React from 'react';
+import {
+	SiGit,
+	SiGithub,
+	SiJava,
+	SiReact,
+	SiCplusplus,
+	SiStyledcomponents as SiStyledComponents,
+	SiTailwindcss,
+
+//	SiTwitter,
+	
+} from 'react-icons/si';
 
 function technologies() {
   return (
     <div className='technologies'>
       
         
-      <h1 class="text-2xl font-bold sm:text-3xl">Technologies 💻 </h1>
-      
-    
+     
+
+      <div className="space-y-7">
+				<h1 className="text-2xl font-bold sm:text-3xl">Technologies 💻</h1>
+
+				<ul className="grid grid-cols-3 gap-4 sm:grid-cols-4 ">
+          <SiReact text="React.js"/>
+          <SiGithub text="GitHub" />
+					<SiJava text="Java" />
+					<SiTailwindcss text="TailwindCSS" />
+					<SiGit text="Git" />
+          <SiCplusplus text="Cpp" />
+					<SiStyledComponents text="styled-components" />
+			
+				</ul>
+			</div>
     </div>
   )
 }
