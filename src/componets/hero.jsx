5c33d31d3ff2209b9ca7d2bba4cdd042479d35ff
Omolemo Tshwaolesele <omolemotswaolesele@gmail.com>
2022-07-04 12:15:50 +0200

    fixed the styling

--- src/componets/hero.jsx
+++ src/componets/hero.jsx
@@ -3,17 +3,18 @@ import React from 'react';
 function Hero() {
   return (
     <div >
-            <h1 class="text-3xl font-bold sm:text-4xl md:text-6xl">Hey👋, I'm <span class="text-blue-700 dark:text-white">JinxScript;</span> </h1>
-                
-            
+      <br></br>
+            <h4 class="text-3xl font-bold ">Hey👋, I'm <span class="text-blue-700 dark:text-white">JinxScript;</span> </h4>
+            <br></br>
             <div className='text-slate-400	'>
                 <li>🎓 First Year Computer Science and Software Engineering Student</li>
                 <li> 💻 Aspiring Software Developer</li>
-                <li> 📰Blogger (Shmurk Magazine </li>
+                <li> 📰Blogger Shmurk Magazine </li>
                 <li> 🎥 Youtuber </li>
                 <li> 🎵 Music Producer  </li>
 
             </div>
+      <br></br>
     </div>
   )
 }
