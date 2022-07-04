13efcdd93a8b32c43b66c3b17b2fcbaceec57075
Omolemo Tshwaolesele <omolemotswaolesele@gmail.com>
2022-07-04 13:52:06 +0200

    footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Nav from './componets/navBar';
 import Hero from './componets/hero';
 import Technologies from './componets/technologies';
+import Footer from './componets/footer';
 
 function App() {
   return (  
@@ -14,6 +15,7 @@ function App() {
                 <Hero/> 
                 <br></br>
                 <Technologies/>
+                <Footer/>
           </div>
           </div>
   )
---
+++ src/componets/footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function footer() {
+  return (
+    <div>
+        <footer class="p-4 py-10 mx-auto mt-20 max-w-3xl border-t-2 border-gray-900/10 dark:border-white/10 opacity-50">
+            <h1 class="text-3xl font-bold">JinxScript</h1><p>Software Engineer • 2022</p>
+            </footer>
+    </div>
+  )
+}
+
+export default footer;
