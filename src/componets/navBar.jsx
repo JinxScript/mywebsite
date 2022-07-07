cd136c108540eb5f93bc919aab7dbe5fa2c5e5c1
Omolemo Tshwaolesele <omolemotswaolesele@gmail.com>
2022-07-07 15:38:35 +0200

    nav bar

--- src/componets/navBar.jsx
+++ src/componets/navBar.jsx
@@ -9,7 +9,7 @@ function navBar() {
             <div className='Navbar-left'>
                 <p className="hidden sm:flex sm:flex-row sm:gap-x-4">
                         <a href="/#" className="cursor-pointer rounded-md px-4 py-[0.10rem] text-xl text-gray-200 duration-100 hover:bg-zinc-800">Home</a>
-                        <a href="/#skills" className="cursor-pointer rounded-md px-4 py-[0.10rem] text-xl text-gray-200 duration-100 hover:bg-zinc-800">Technologies</a>
+                        <a href="/#skills" className="cursor-pointer rounded-md px-4 py-[0.10rem] text-xl text-gray-200 duration-100 hover:bg-zinc-800">Skills</a>
                         <a href="/#projects" className="cursor-pointer rounded-md px-4 py-[0.10rem] text-xl text-gray-200 duration-100 hover:bg-zinc-800">Projects</a>
                  </p>
             </div>
